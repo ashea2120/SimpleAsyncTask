86c3c0f320bd7c67f868793bd5252117600902fb
ashea2120 <ashea2120@yahoo.com>
2019-05-16 11:20:44 -0400

    added code for the AsyncTask subclass

--- app/src/main/java/com/example/simpleasynctask/MainActivity.java
+++ app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.simpleasynctask;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textView1);
+
     }
 
     public void startTask(View view) {
+        mTextView.setText(R.string.napping);
+
+        new SimpleAsyncTask(mTextView).execute();
     }
+
+
 }
+
